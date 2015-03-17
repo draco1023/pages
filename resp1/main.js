ebe57560d06b442520831aeb59d57a703c3a69fd
Chang Peng <draco1023@gmail.com>
2015-03-17 16:43:06 +0800

    arrow down

--- resp1/main.js
+++ resp1/main.js
@@ -2,7 +2,7 @@
 * @Author: Draco
 * @Date:   2015-03-03 14:45:05
 * @Last Modified by:   Administrator
-* @Last Modified time: 2015-03-17 16:22:46
+* @Last Modified time: 2015-03-17 16:47:43
 */
 
 function easeRepeat(easing, times) {
