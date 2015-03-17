4393dc3d74407b9d4d6faa5f338dec31ac8eb75a
Chang Peng <draco1023@gmail.com>
2015-03-17 13:45:26 +0800

    resp1 all ready

--- resp1/main.js
+++ resp1/main.js
@@ -2,7 +2,7 @@
 * @Author: Draco
 * @Date:   2015-03-03 14:45:05
 * @Last Modified by:   Administrator
-* @Last Modified time: 2015-03-13 17:15:53
+* @Last Modified time: 2015-03-17 13:16:50
 */
 
 function easeRepeat(easing, times) {
@@ -23,6 +23,7 @@ defEase = mina.easeinout,
 jqCache = {},
 svgCache = {},
 timeouts = [],
+minas = [],
 states = {
     '#img1-1': {
         o: {
@@ -310,7 +311,7 @@ states = {
             o: 1,
             n: .7
         },
-        t: defDuration * 2.7
+        t: defDuration * 2.1
     },
     '#g6-3': {
         o: {
@@ -319,7 +320,7 @@ states = {
         n: {
             transform: 'translate(0,200)'
         },
-        t: defDuration * 2.7
+        t: defDuration * 2.1
     },
     '#text6-1': {
         o: {
@@ -330,7 +331,7 @@ states = {
             transform: 'translate(0,0)',
             'fill-opacity': 1
         },
-        t: defDuration * 3.6
+        t: defDuration * 2.5
     },
     '#img7-1': {
         o: {
@@ -446,6 +447,325 @@ states = {
         t: defDuration * 3,
         q: 1,
         e: mina.elastic
+    },
+    '.text10-1': {
+        o: {
+            transform: 'translate(0,-200)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        },
+        q: 1,
+        qd: 500,
+        t: 25
+    },
+    '#img10-1': {
+        scale: {
+            o: 0,
+            n: 1
+        },
+        t: defDuration * 1.8,
+        e: mina.backout
+    },
+    '#img10-2': {
+        o: {
+            opacity: 0
+        },
+        n: {
+            opacity: 1
+        },
+        t: defDuration * 2.7
+    },
+    '#g11-1': {
+        o: {
+            transform: 'translate(0,-200)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        }
+    },
+    '#img11-1': {
+        o: {
+            opacity: 0
+        },
+        n: {
+            opacity: 1
+        },
+        t: defDuration
+    },
+    '.text11-1': {
+        o: {
+            transform: 'translate(-500,0)',
+            opacity: 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            opacity: 1
+        },
+        t: defDuration * 2,
+        q: 1,
+        d: 700,
+        qd: 500
+    },
+    '#g12-1': {
+        o: {
+            transform: 'translate(0,-200)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        }
+    },
+    '#g12-2': {
+        o: {
+            opacity: 0
+        },
+        n: {
+            opacity: 1
+        },
+        t: defDuration
+    },
+    '.text12-1': {
+        o: {
+            transform: 'translate(-500,0)',
+            opacity: 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            opacity: 1
+        },
+        t: defDuration * 2,
+        q: 1,
+        d: 700,
+        qd: 500
+    },
+    '#g13-1': {
+        o: {
+            transform: 'translate(0,-200)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        }
+    },
+    '.img13-2': {
+        scale: {
+            o: 0,
+            n: 1
+        },
+        t: defDuration * 1.25,
+        q: 1,
+        d: 650,
+        e: mina.elastic
+    },
+    '#img13-2': {
+        o: {
+            transform: 'translate(0,-300)',
+            opacity: 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            opacity: 1
+        },
+        t: defDuration * 5,
+        e: mina.backout
+    },
+    '#g14-1': {
+        o: {
+            transform: 'translate(0,-300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        },
+        e: mina.backout
+    },
+    '#img14-1': {
+        o: {
+            opacity: 0
+        },
+        n: {
+            opacity: 1
+        },
+        t: defDuration
+    },
+    '#img14-2': {
+        o: {
+            opacity: 0
+        },
+        n: {
+            opacity: 1
+        },
+        t: defDuration + 700
+    },
+    '.text15-1': {
+        o: {
+            transform: 'translate(0,-300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        },
+        e: mina.bounce
+    },
+    '#img15-1': {
+        o: {
+            opacity: 0
+        },
+        n: {
+            opacity: 1
+        },
+        t: defDuration
+    },
+    '#g16-1': {
+        o: {
+            transform: 'translate(0,-300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        }
+    },
+    '#rect16-1': {
+        o: {
+            height: 872
+        },
+        n: {
+            height: 0
+        },
+        d: defDuration * 4,
+        t: defDuration / 2,
+        e: mina.linear
+    },
+    '.g16-2': {
+        scale: {
+            o: 0,
+            n: 1
+        },
+        t: defDuration,
+        q: 1,
+        qd: defDuration * 2 / 3,
+        e: mina.elastic
+    },
+    '#g17-1': {
+        o: {
+            transform: 'translate(0,-300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        }
+    },
+    '#rect17-1': {
+        o: {
+            x: 0
+        },
+        n: {
+            x: 800
+        },
+        t: defDuration,
+        d: defDuration * 2
+    },
+    '#g18-1': {
+        o: {
+            transform: 'translate(0,-300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        }
+    },
+    '#rect18-1': {
+        o: {
+            height: 768
+        },
+        n: {
+            height: 0
+        },
+        t: defDuration,
+        d: defDuration * 2
+    },
+    '.img19-1': {
+        scale: {
+            o: 0,
+            n: 1
+        },
+        t: 100,
+        q: 1,
+        qd: 500,
+        e: mina.elastic
+    },
+    '.text19-1': {
+        o: {
+            transform: 'translate(0,300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        },
+        t: 2500,
+        q: 1,
+        qd: 700,
+        e: mina.backout
+    },
+    '.img20-1': {
+        scale: {
+            o: 0,
+            n: 1
+        },
+        t: 100,
+        q: 1,
+        qd: 500,
+        e: mina.elastic
+    },
+    '.text20-1': {
+        o: {
+            transform: 'translate(0,300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        },
+        t: 2500,
+        q: 1,
+        qd: 700,
+        e: mina.backout
+    },
+    '#g21-1': {
+        o: {
+            transform: 'translate(0,-300)',
+            'fill-opacity': 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            'fill-opacity': 1
+        },
+        e: mina.bounce
+    },
+    '#rect21-1': {
+        o: {
+            transform: 'translate(0,900)',
+            opacity: 0
+        },
+        n: {
+            transform: 'translate(0,0)',
+            opacity: 1
+        },
+        t: defDuration,
+        d: defDuration * 2
     }
 },
 components = [
@@ -458,17 +778,18 @@ components = [
     ['#img7-1', '.img7-2', '#g7-2'],
     ['#g8-1', '#img8-1', '#rect8-1', '#img8-2'],
     ['#g9-1', '.img9-1', '.g9-2'],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    []
+    ['.text10-1', '#img10-1', '#img10-2'],
+    ['#g11-1', '#img11-1', '.text11-1'],
+    ['#g12-1', '#g12-2', '.text12-1'],
+    ['#g13-1', '.img13-2', '#img13-2'],
+    ['#g14-1', '#img14-1', '#img14-2'],
+    ['.text15-1', '#img15-1'],
+    ['#g16-1', '#rect16-1', '.g16-2'],
+    ['#g17-1', '#rect17-1'],
+    ['#g18-1', '#rect18-1'],
+    ['.img19-1', '.text19-1'],
+    ['.img20-1', '.text20-1'],
+    ['#g21-1', '#rect21-1']
 ];
 
 function _(selector, isJquery) {
@@ -518,7 +839,7 @@ function trans(el, state, duration, ease, callback) {
     xy = state.translate,
     bbox = r || s || xy ? el.getBBox() : null;
     if (bbox) {
-        Snap.animate(0, 1, function(v) {
+        var a = Snap.animate(0, 1, function(v) {
             var matrix = new Snap.Matrix();
             if (r) {
                 var angle = r.o + (r.n - r.o) * v;
@@ -535,6 +856,7 @@ function trans(el, state, duration, ease, callback) {
             }
             el.transform(matrix);
         }, duration || 0, ease, callback);
+        minas.push(a);
     }
 }
 
@@ -563,6 +885,8 @@ function reverse(el, state) {
 function attr(selector) {
 	var els = _(selector),
     s = states[selector];
+    if (!els)
+        return;
 	if (els.stop) {
 		els.stop().attr(s.o || {});
         reverse(els, s);
@@ -619,7 +943,8 @@ function animate(selector, duration, ease, callback) {
     e = ease || s.e || defEase,
     t = s.t || 0;
     els = _(selector);
-
+    if (!els)
+        return;
     if (t) {
         timeout(els, s, t, d, qd, e, callback);
     } else {
@@ -689,6 +1014,25 @@ $(function() {
             for (var j = 0, l = comp.length; j < l; j++) {
             	animate(comp[j]);
             };
+            if (index === 13) {
+                var pen = _('#img13-1'),
+                path = _('#path13-1'),
+                len = path.getTotalLength();
+
+                timeouts.push(setTimeout(function() {
+                    minas.push(Snap.animate(0, 1, function(v) {
+                        var pos = path.getPointAtLength(len * v);
+                        pen.attr({
+                            x: pos.x,
+                            y: pos.y
+                        });
+                    }, 4000, mina.linear, function() {
+                        pen.animate({
+                            opacity: 0
+                        }, 500);
+                    }));
+                }, defDuration));
+            }
 
             if (ua.match(/iPhone|iPad|iPod/i)) {
             	// var di = dataIndex(index);
@@ -702,8 +1046,19 @@ $(function() {
         	while (timeouts.length) {
         		clearTimeout(timeouts.pop());
         	}
+            while (minas.length) {
+                minas.pop().stop();
+            }
+
             // if (index == 1 && direction == 'down') {
             // }
+            if (index === 13) {
+                _('#img13-1').stop().attr({
+                    x: 120.4,
+                    y: 289.6,
+                    opacity: 1
+                });
+            }
             var comp = getComponent(index);
             for (var j = comp.length - 1; j >= 0; j--) {
             	attr(comp[j]);
